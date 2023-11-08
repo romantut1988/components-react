e894671c30df07ec1a7bd19ec6551af725b3558a
Roman <romantut1988@mail.ru>
2023-11-08 06:34:55 +0300

    functionality component start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import './App.css';
-import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
 
 function App() {
     return (
-        <div>
-            <PageTitle tite={"This is APP component"}/>
-            Article 1
-            <Rating value={3}/>
-            <Accordion/>
-            Article 2
-            <Rating value={4}/>
-            <Rating value={0}/>
-            <Rating value={5}/>
-        </div>
+        <h1>HELLO</h1>
     );
-}
-
-function PageTitle(props: any) {
-    return <h1>{props.title}</h1>
 }
 
 
